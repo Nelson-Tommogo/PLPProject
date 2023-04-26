9bea4ed5b228595cc641d964a26322177a18531b
Tommogo-coder <nelsontommogo9@gmail.com>
2023-04-27 00:18:50 +0300

    PLP Bootcamp

---
+++ createTask.js
@@ -0,0 +1,21 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyB-SYMYbg8XeN0ELp07Cxa_6WJZOc34h34",
+
+  authDomain: "authenticationapp-afee7.firebaseapp.com",
+
+  databaseURL: "https://authenticationapp-afee7-default-rtdb.firebaseio.com",
+
+  projectId: "authenticationapp-afee7",
+
+  storageBucket: "authenticationapp-afee7.appspot.com",
+
+  messagingSenderId: "555290974806",
+
+  appId: "1:555290974806:web:46b97adab9fb874c5979f7"
+
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
